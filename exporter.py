952075185391335c985e525e21cc1a84a9815f4c
andrew <anaylor13@yahoo.com>
2025-06-14 22:58:41 +0000

    added more columns for larger model

--- exporter.py
+++ exporter.py
@@ -5,20 +5,20 @@ import requests
 import json
 import time
 import os
+from datetime import datetime
 
 PROMETHEUS_HOST=os.environ["PROMETHEUS_HOST"]
-AWS_ACCESS_KEY_ID=os.environ["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY=os.environ["AWS_SECRET_ACCESS_KEY"]
-MLFLOW_S3_ENDPOINT_URL=os.environ["MLFLOW_S3_ENDPOINT_URL"]
 LOGGED_MODEL=os.environ["LOGGED_MODEL"]
-NETWORK_ANOMALY = Gauge('node_network_receive_bytes_total_anomaly', 'Anomaly Score of Network Rx')
-PROM_QUERY = f"http://{PROMETHEUS_HOST}:9090/api/v1/query?query=node_network_receive_bytes_total"
+NETWORK_ANOMALY = Gauge('node_network_anomaly', 'Anomaly Score of Network Data')
 
-def get_network_data():
-    resp = requests.get(PROM_QUERY)
+def get_network_data(metric, rate_base, suffix):
+    prom_query = f'http://{PROMETHEUS_HOST}:9090/api/v1/query?query=rate({metric}[{rate_base}])'
+    resp = requests.get(prom_query)
     input_data = json.loads(resp.text)
     df = pd.DataFrame({x['metric']['device']: [float(x['value'][1])] for x in input_data['data']['result']})
-    df = df[['br-0c6e79cd156e', 'br-8fbdbc8e9f6a', 'docker0', 'eth0', 'lo']]
+    for column in df.columns:
+        df.rename(columns={column: column + suffix}, inplace=True)
+    df = df[['br-0c6e79cd156e' + suffix, 'br-8fbdbc8e9f6a' + suffix, 'docker0' + suffix, 'eth0' + suffix, 'lo' + suffix]]
     return df
 
 
@@ -31,6 +31,14 @@ if __name__ == '__main__':
     # Load model as a PyFuncModel.
     loaded_model = mlflow.pyfunc.load_model(LOGGED_MODEL)
     while True:
-        input_data = get_network_data()
-        NETWORK_ANOMALY.set(loaded_model.predict(input_data))
+        node_network_receive_bytes_total = get_network_data('node_network_receive_bytes_total', '1m',  ' - Rx in')
+        node_network_transmit_bytes_total = get_network_data('node_network_transmit_bytes_total', '1m',  ' - Tx in')
+        node_network_receive_packets_total = get_network_data('node_network_receive_packets_total', '1m',  ' - Pck in')
+        node_network_transmit_packets_total = get_network_data('node_network_transmit_packets_total', '1m',  ' - Pck out')
+
+        network_df = pd.concat([node_network_receive_bytes_total, node_network_transmit_bytes_total], axis=1)
+        network_df = pd.concat([network_df, node_network_receive_packets_total], axis=1)
+        network_df = pd.concat([network_df, node_network_transmit_packets_total], axis=1)
+        network_df = network_df[network_df.columns.sort_values()]
+        NETWORK_ANOMALY.set(loaded_model.predict(network_df))
         time.sleep(30)
